a93a19d44d556d395cb2eff0a07ff12513a079f5
Onix <piyvoda20033@gmail.com>
2026-02-04 18:20:18 +0200

    some amount completed

--- module-08/scripts/app.js
+++ module-08/scripts/app.js
@@ -1,2 +1,68 @@
 // TODO: Get posts info
 // console.log(posts);
+
+const list = document.querySelector(".cards");
+const loader = document.querySelector(".spinner");
+const form = document.querySelector(".search-form");
+
+function renderPosts(posts) {
+  return posts
+    .map(({ id, title, body, tags, reactions, views, userId, coverImage }) => {
+      return `<li class="card border-dark mb-3" data-userid="${userId}">
+            <div class="card-body">
+              <h5 class="card-title">${title}</h5>
+              <hr />
+              <div class="card-meta mb-3">
+                <div class="card-meta-tags">
+                  <p class="m-0 text-black-50 fw-lighter">
+                    <i class="bi bi-tag"></i>
+                  </p>
+                  <ul class="tags-list">
+                    <li
+                      class="border border-dark rounded-1 text-center text-black-50 fw-lighter"
+                    >
+                      <small>${tags[0]}</small>
+                    </li>
+                    <li
+                      class="border border-dark rounded-1 text-center text-black-50 fw-lighter"
+                    >
+                      <small>${tags[1]}</small>
+                    </li>
+                    <li
+                      class="border border-dark rounded-1 text-center text-black-50 fw-lighter"
+                    >
+                      <small>${tags[2]}</small>
+                    </li>
+                  </ul>
+                </div>
+
+                <ul class="card-meta-likes">
+                  <li class="text-black-50 fw-lighter">
+                    <i class="bi bi-hand-thumbs-up"></i> <small>${reactions.likes}</small>
+                  </li>
+                  <li class="text-black-50 fw-lighter">
+                    <i class="bi bi-hand-thumbs-down"></i> <small>${reactions.dislikes}</small>
+                  </li>
+                </ul>
+
+                <p class="card-meta-views m-0 text-black-50 fw-lighter">
+                  <i class="bi bi-eye"></i> <small>${views}</small>
+                </p>
+              </div>
+              <hr />
+              <p class="card-text">
+                ${body}
+              </p>
+            </div>
+          </li>`;
+    })
+    .join("");
+}
+
+function handleFormSubmit() {}
+
+form.addEventListener("submit", handleFormSubmit);
+
+loader.classList.remove("visually-hidden");
+const markup = renderPosts(posts);
+list.insertAdjacentHTML("beforeend", markup);
