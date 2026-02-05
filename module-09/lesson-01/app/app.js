0a20df54969be8ee515d2179d924ce80b7e88614
Onix <piyvoda20033@gmail.com>
2026-02-05 18:24:38 +0200

    module 9 lesson 1 completed

--- module-09/lesson-01/app/app.js
+++ module-09/lesson-01/app/app.js
@@ -6,6 +6,7 @@ let store = {
 
 // Generate id instance
 const g = new GenerateId();
+const STORE_KEY = { notes: "notes", appTheme: "theme" };
 
 // Elements
 const listEl = document.querySelector(".js-notes-list");
@@ -15,6 +16,18 @@ const deleteBtn = document.querySelector(".js-delete-btn");
 
 // Listeners
 formEl.addEventListener("submit", onFormSubmit);
+listEl.addEventListener("click", onDeleteBtnClick);
+
+// Initial code
+function init() {
+  const savedNotes = loadLS(STORE_KEY.notes);
+  if (!savedNotes) {
+    return;
+  }
+  store.notes = [...savedNotes];
+  renderNotes(store.notes);
+}
+init();
 
 // Functions
 function onFormSubmit(event) {
@@ -26,13 +39,21 @@ function onFormSubmit(event) {
     ...Object.fromEntries(new FormData(form)),
     id: g.generate(10),
   };
+
+  if (notesData.name === "" || notesData.body === "") {
+    console.log("Empty field!!");
+    return;
+  }
+
   store.notes = [...store.notes, notesData];
+  renderNotes(store.notes);
+  saveLS(STORE_KEY.notes, store.notes);
   form.reset();
 }
 
 function renderNotes(notes = []) {
   const markup = notes.map(createNote).join("");
-  listEl.insertAdjacentHTML("beforeend", markup);
+  listEl.innerHTML = markup;
 }
 
 function createNote({ id, name, body }) {
@@ -53,4 +74,34 @@ function createNote({ id, name, body }) {
             </div>
           </li>
     `;
+}
+function onDeleteBtnClick(event) {
+  if (!event.target.classList.contains("btn")) {
+    return;
+  }
+  const noteId = event.target.dataset.noteid;
+  const filtredNotes = store.notes.filter(note => note.id !== noteId);
+
+  store.notes = [...filtredNotes];
+
+  saveLS(STORE_KEY.notes, store.notes);
+  renderNotes(store.notes);
+}
+
+// LOCAL STORAGE FUNCTIONS
+function saveLS(key, value) {
+  try {
+    const serialisedState = JSON.stringify(value);
+    localStorage.setItem(key, serialisedState);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+function loadLS(key) {
+  try {
+    const serialisedState = localStorage.getItem(key);
+    return JSON.parse(serialisedState);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
